bcb19616dbd841781274627496c9ee3ca1533f7d
atul-koshta <atulk2018@gmail.com>
2026-04-29 13:57:30 +0530

    resume v1.0.0 - tool: https://rendercv.com

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -37,8 +37,8 @@ export default function RootLayout({
           <Footer />
         </ThemeProvider>
         <script
-          src="https://widget-dev.cateringrewards.com/loader.js"
-          data-cr-partner-key="wp_602a2c7bb789c64ca34995598dfb9d6c"
+          src="https://widget-staging.cateringrewards.com/loader.js"
+          data-cr-partner-key="wp_2f1fee579c4f220c9a4b965b7b7c959d"
           data-cr-position="bottom-right"
           async
         />
